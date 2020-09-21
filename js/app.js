cc1e98b59e0b3831280247ed21fb2a4902ad4359
MateusSoares <mateussoares114@gmail.com>
2020-09-20 21:02:48 -0300

    mudança de Layout,correções de erros, melhorias adicionais - v2.1.1

--- js/app.js
+++ js/app.js
@@ -13,87 +13,145 @@ var app = new Framework7({
     routes: [
       {
         path: '/home/',
-        url: 'index.html',
+        url: 'index.html?a=x',
+      },
+      {
+        path: '/historico/',
+        url: 'historico.html?a=x',
+        on:{
+          pageInit:function(){
+            historico();
+          },
+        },
       },
     ],
     // ... other parameters
   });
   var mainView = app.views.create('.view-main');
 
-  // Inicializando o JS
-  $(document).ready(function(){
-    $('#btn-calcular').click(function(){
-      var peso = $('#peso').val();
-      var altura = $('#altura').val();
+  if(window.openDatabase){
+    // Criando banco de dados
+    db = openDatabase("DB_COMBUSTIVEL","0.1","Base de dados local", 5*1021*1024);
 
-      if(peso == "" || altura == ""){
-        app.dialog.alert('Digite o peso e altura','AVISO');
+    // Criando tabela tarefas
+    db.transaction(function(query){
+      query.executeSql("CREATE TABLE IF NOT EXISTS oleo (id INTEGER PRIMARY KEY AUTOINCREMENT, dataTroca date, dataPtroca date)");
+    
+      // Gravando configurações
+      query.executeSql("CREATE TABLE IF NOT EXISTS abastecimento (id INTEGER PRIMARY KEY AUTOINCREMENT, dia date, valor INTEGER)");  // INTEGER = Inteiro
+    
+    });
+  }
+
+  function historico(){
+    $('#btn-oleo').click(function(){
+      var dataTroca = $("#dataTroca").val();
+      var dataPtroca = $("#dataPtroca").val();
+
+      if(dataTroca == "" || dataPtroca == ""){
+        app.dialog.alert("Os campos não podem ficar vazios","AVISO");
         return false;
       }
 
-      // Converter os textos para Float
-      peso = parseFloat(peso);
-      altura = parseFloat(altura);
-
-      resultIMC = peso / (altura * altura);
-      resultIMC = resultIMC.toFixed(2);
-
-      $('#peso').val("");
-      $('#altura').val("");
-      
-      var men = "";
-      // Verificando a classificação do IMC
-      if(resultIMC <= 18.5){
-        men = "Cuidado! Você está muito abaixo do peso.";
-        color = "#990000";
-      }else if(resultIMC >18.5 && resultIMC <=24.99){
-        men = "Parabéns! Peso ideal (continue assim).";
-        color = "#4cd964";
-      }else if(resultIMC >24.99 && resultIMC <=29.99){
-        men = "Olha só! Você esta um pouco acima do peso.";
-        color = "#ff9800";
-      }else if(resultIMC >29.99 && resultIMC <=34.99){
-        men = "Atenção! Obesidade grau I, Comece a fazer mais exercícios.";
-        color = "#7b5208";
-      }else if(resultIMC >34.99 && resultIMC <=39.99){
-        men = "Atenção! Obesidade grau II(Severa). Procure um médico urgente.";
-        color = "#17a1e4";
-      }else{
-        men = "Atenção! Obesidade grau III(Mórbida). Procure um médico urgente.";
-        color = "#990000";
+      if(dataTroca < today || dataPtroca < today){
+        app.dialog.alert("A data não pode ser retroativa","AVISO");
+        $("#dataTarefa,#dataPtroca").val(today);
+        return false;
       }
 
-      // app.dialog.alert('Seu IMC é '+ resultIMC.replace('.',',') +'<br><br>'+ men,'AVISO');
-    
-      demoGauge.update({
-        value:resultIMC / 50,
-        borderColor: color,
-        labelText:"Indice de Massa Corporal",
-        valueText: resultIMC.replace('.',',')
-      })
-    
-      $('.men').html(men);
+      var notification = app.notification.create({
+        title:'Troca de óleo',
+        text:'Informações inseridas com sucesso',
+        closeTimeout:3000,
+      });
+      // Inserindo informações no banco
+      db.transaction(function(query){
+        query.executeSql("INSERT INTO oleo (dataTroca,dataPtroca) VALUES (?,?)",[dataTroca,dataPtroca]);
+        notification.open();
+        $("#dataTarefa,#dataPtroca").val(today);
+      });
 
     });
-  });
 
-  // Init top demo gauge
-var demoGauge = app.gauge.create({
-  el: '.demo-gauge',
-  type: 'semicircle',
-  value: 0.5,
-  size: 250,
-  borderColor: '#2196f3',
-  borderWidth: 10,
-  valueText: '50%',
-  valueFontSize: 41,
-  valueTextColor: '#2196f3',
-  labelText: '?',
-});
+    $('#btn-combus').click(function(){
+      var dia = $("#dia").val();
+      var valorAbast = $("#valorAbast").val();
+
+      if(dia == "" || valorAbast == ""){
+        app.dialog.alert("Os campos não podem ficar vazios","AVISO");
+        return false;
+      }
+
+      if(dia < today){
+        app.dialog.alert("A data não pode ser retroativa","AVISO");
+        $("#dia").val(today);
+        return false;
+      }
+
+      var notification = app.notification.create({
+        title:'Abastecimento',
+        text:'Informações inseridas com sucesso',
+        closeTimeout:3000,
+      });
+      // Inserindo informações no banco
+      db.transaction(function(query){
+        query.executeSql("INSERT INTO abastecimento (dia,valor) VALUES (?,?)",[dia,valorAbast]);
+        notification.open();
+        $("#dia").val(today);
+        $("#valorAbast").val("");
+      });
+
+    });
+  }
 
 // Calculo combustivel
 
 $(document).ready(function(){
+
+  dados = "";
+  db.transaction(function(query){
+    query.executeSql("SELECT * FROM oleo",[],function(query,result){
+      linha = result.rows;
+      for(var i = 0; i < linha.length; i++){
+
+        dataT = linha[i].dataTroca;    // formatando a data
+        split = dataT.split('-');
+        novaData = split[2]+"/"+split[1]+"/"+split[0];
+
+        dataP = linha[i].dataPtroca;    // formatando a data
+        split2 = dataP.split('-');
+        novaData2 = split2[2]+"/"+split2[1]+"/"+split2[0];
+
+        dados+='<td class="label-cell">'+novaData+'</td>';
+        dados+='<td class="label-cell">'+novaData2+'</td>';
+      }
+
+      $(".resultOleo").html(dados);
+    });
+  });
+
+  dados2 = "";
+  resultGastos = "";
+  db.transaction(function(query){
+    query.executeSql("SELECT * FROM abastecimento",[],function(query,result){
+      linha = result.rows;
+      for(var i = 0; i < linha.length; i++){
+
+        dataD = linha[i].dia;    // formatando a data
+        split = dataD.split('-');
+        novaData = split[2]+"/"+split[1];
+
+        dados2+='<td class="label-cell">'+novaData+'</td>';
+        dados2+='<td class="label-cell">R$ '+linha[i].valor+'</td>';
+
+        resultGastos = 0 + linha[i].valor;
+      }
+
+      $(".resultCombus").html(dados2);
+      $(".total-gastos").html("R$ "+resultGastos);
+    });
+  });
+
   $('.btn-calcular').click(function(){
     etanol = $('#etanol').val();
     gasolina = $('#gasolina').val();
@@ -103,6 +161,14 @@ $(document).ready(function(){
     // Verificar se foi digitado os valores
     if(etanol == "" || gasolina == ""){
       app.dialog.alert('Informe o preço do Etanol e Gasolina','AVISO');
+      return false;
+    }else if(etanol == 0 || gasolina == 0){
+      app.dialog.alert('Os campos não podem ter valores 0','AVISO');
+      return false;
+    }
+
+    if(etanol.length > 5 || gasolina.length > 5){
+      app.dialog.alert('Os campos não podem ter mais de 4 dígitos','AVISO');
       return false;
     }
 
@@ -129,9 +195,10 @@ $(document).ready(function(){
 
   // Acionando botão novo
   $('.btn-novo').click(function(){
-    $('#etanol, #gasolina,#dinheiro,#litros').val("");
+    $('#etanol, #gasolina,#dinheiro,#litros,#kml,#distancia,#valorCombus').val("");
     $('.mensagem, .porc').html("");
     $('.btn-calcular,#etanol,#gasolina').attr('disabled',false);
+    $(".consumo, .totalLitros, .gastos").hide();
     desabilitar();
   });
 
@@ -141,6 +208,11 @@ $(document).ready(function(){
     $('.btn-novo').attr('disabled',false);
     $('.btn-calcular,#etanol,#gasolina').attr('disabled',true);
     dinheiro = $('#dinheiro').val();
+
+    if(dinheiro.length > 3){
+      app.dialog.alert('Os campos não podem ter mais de 4 dígitos','AVISO');
+      return false;
+    }
 
     if(dinheiro > 0){
       $('#litros').attr('disabled', true);
@@ -171,6 +243,12 @@ $(document).ready(function(){
     $('.btn-calcular,#etanol,#gasolina').attr('disabled',true);
 
     litros = $('#litros').val();
+
+    if(litros.length > 3){
+      app.dialog.alert('Os campos não podem ter mais de 3 dígitos','AVISO');
+      return false;
+    }
+
     if(litros > 0){
       $('#dinheiro').attr('disabled',true);
       litros = parseFloat(litros);
@@ -200,10 +278,19 @@ $(document).ready(function(){
     var cidade = $('#cidade').val();
     cidade = parseFloat(cidade);
 
+    if(cidade == 0){
+      app.dialog.alert('Os campos não podem ter valores 0','AVISO');
+      return false;
+    }
+
     if(litros != ""){
       resultCidade = litros * cidade;
     }
     $('.m-cidade').html(resultCidade.toFixed(2)+" Km na cidade");
+
+    if(cidade == ""){
+      $('.m-cidade').html("");
+    }
   });
 
   // Verifica o valor do input pista
@@ -211,10 +298,52 @@ $(document).ready(function(){
     var pista = $('#pista').val();
     pista = parseFloat(pista);
 
+    if(pista == 0){
+      app.dialog.alert('Os campos não podem ter valores 0','AVISO');
+      return false;
+    }
+
     if(litros != ""){
       resultPista = litros * pista;
     }
+
     $('.m-pista').html(resultPista.toFixed(2)+" Km na pista");
+
+  });
+
+  $('.btn-viagem').click(function(){
+    kml = $('#kml').val();
+    distancia = $('#distancia').val();
+    valorCombus = $('#valorCombus').val();
+
+    // Verificar se foi digitado os valores
+    if(kml == "" || distancia == "" || valorCombus == ""){
+      app.dialog.alert('Os campos não podem ficar vazios','AVISO');
+      return false;
+    }else if(kml == 0 || distancia == 0 || valorCombus == 0){
+      app.dialog.alert('Os campos não podem ter valores 0','AVISO');
+      return false;
+    }
+
+    // Converter os valores do input
+    kml = parseFloat(kml);
+    distancia = parseFloat(distancia);
+    valorCombus = parseFloat(valorCombus);
+
+    combustivel = distancia / kml;
+
+    gastos = combustivel * valorCombus;
+
+    gastos = gastos.toFixed(2);
+
+    $('.resultLitros').html(combustivel+"L");
+    $('.resultGastos').html("R$ "+gastos);
+
+    $('.totalLitros,.gastos').show();
+  });
+
+  $('#consumo,#distancia,#valorCombus').on('input',function(){
+    $('.btn-novo').attr('disabled',false);
   });
 
 });
@@ -222,5 +351,5 @@ $(document).ready(function(){
 function desabilitar(){
   // Desabilitar segunda parte
   $('.btn-novo,#dinheiro,#litros').attr('disabled',true);
-  $('.litros-visible, .dinheiro-visible').hide();
+  $('.litros-visible, .dinheiro-visible, .totalLitros, .gastos').hide();
 }
